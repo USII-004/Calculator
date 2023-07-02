a208459a6fd646502e1a3a1a3e430f7857b8e2d5
USII-004 <usmansmooth004@gmail.com>
2023-07-02 02:49:59 +0100

    functional calculator

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 A basic functional calculator app. Functionalities have been implemented using javaScript. This project will help me test out my JS skills
 
 basic web design has already been created using HTML and CSS, Funtionalities will be added using javaScript as i begin learning about object oriented programming using JS.
+
+this calculator now works well, but there is still an error noticablel when we repeatedly press the "." (decimal point button) repeatedly, this should not be so. i need to make the calculator so that only one decimal point should be displayed (includes function do not work as this would have made it pretty easy to use an if statement)
+
+# Scientific calculator
+All i want to include a toggle that will turn the calculator to a scientific calculator, but  i know this is going to require a more knowledge of OOP.  
--- script.js
+++ script.js
@@ -1,25 +1,49 @@
 class Calculator {
-  constructor (previousOperandDisplay, currentOperandDisplay) {
-    this.previousOperandDisplay = previousOperandDisplay;
+  constructor (currentOperandDisplay) {
     this.currentOperandDisplay = currentOperandDisplay;
+
+    this.clear = function() {
+      this.currentOperandDisplay.innerText = '';
+    }
 
     this.appendNumber = function(number) {
       this.currentOperand =  number;
     }
 
     this.updateDisplay = function() {
+      if (currentOperandDisplay.innerText == "." && currentOperandDisplay.innerText.includes('.')) {
+        return;
+      }
       this.currentOperandDisplay.innerText += this.currentOperand;
     }
+
+    this.chooseOperation = function(num) {
+      this.currentOperand = num;
+    }
+
+    this.compute = function() {
+      try {
+        this.currentOperandDisplay.innerText = eval(currentOperandDisplay.innerText); 
+      } catch (error) {
+        alert("Invalid operation");
+      }
+    }
+
+    this.percentage = function() {
+      this.currentOperandDisplay.innerText = eval(currentOperandDisplay.innerText/100);
+    }
+
   }
+  
 }
 let numberButtons = document.getElementsByClassName('number');
 let operationButtons = document.getElementsByClassName('operation');
 let allClearButton = document.querySelector('.all-clear');
 let equalButton = document.querySelector('.equal');
-let previousOperandDisplay = document.querySelector('.upper-display');
-let currentOperandDisplay = document.querySelector('.lower-display');
+let percentageButton = document.querySelector('.percentage');
+let currentOperandDisplay = document.querySelector('.display');
 
-let calculator = new Calculator(previousOperandDisplay, currentOperandDisplay);
+let calculator = new Calculator(currentOperandDisplay);
 
 
 for (let i = 0; i < numberButtons.length; i++) {
@@ -29,3 +53,23 @@ for (let i = 0; i < numberButtons.length; i++) {
     calculator.updateDisplay();
   });
 }
+
+for (let i = 0; i < operationButtons.length; i++) {
+  let button = operationButtons[i];
+  button.addEventListener('click', () => {
+    calculator.chooseOperation(operationButtons[i].innerText);
+    calculator.updateDisplay();
+  }) 
+}
+
+allClearButton.addEventListener('click', () => {
+  calculator.clear();
+})
+
+equalButton.addEventListener('click', () => {
+  calculator.compute();
+})
+
+percentageButton.addEventListener('click', () => {
+  calculator.percentage();
+})
